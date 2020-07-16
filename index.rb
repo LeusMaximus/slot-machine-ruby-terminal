699452e5a653faf4e92f0ab9c02152d035c453f7
LeusMaximus <leus.maximus@gmail.com>
2020-07-16 16:12:13 +0300

    Calculate money in one place. Use unified message

--- index.rb
+++ index.rb
@@ -16,6 +16,8 @@ if age >= 18 && play == 'Y'
     print "Press enter to start machine... "
     gets
 
+    prize = 0
+
     x = rand(0..5)
     y = rand(0..5)
     z = rand(0..5)
@@ -24,38 +26,40 @@ if age >= 18 && play == 'Y'
 
     # 000
     if x == 0 && y == 0 && z == 0
-      puts "You lost all money."
-      money = 0
+      prize = -money
     end
 
     # 111
     if x == 1 && y == 1 && z == 1
-      "You have earned 10 dollars."
-      money += 10
+      prize = 10
     end
 
     # 222
     if x == 2 && y == 2 && z == 2
-      "You have earned 20 dollars."
-      money += 20
+      prize = 20
     end
 
     # 333
     if x == 3 && y == 3 && z == 3
-      "You have earned 30 dollars."
-      money += 30
+      prize = 30
     end
 
     # 444
     if x == 4 && y == 4 && z == 4
-      "You have earned 40 dollars."
-      money += 40
+      prize = 40
     end
 
     # 555
     if x == 5 && y == 5 && z == 5
-      "You have earned 50 dollars."
-      money += 50
+      prize = 50
+    end
+
+    money += prize
+
+    if prize >= 0
+      puts "You have earned #{prize} dollars."
+    else
+      puts "You lost #{prize} dollars."
     end
 
     puts "Balance: #{money} dollars."
